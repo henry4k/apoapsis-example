e5bba711807ca836ec3706425734715f541da74f
Henry Kielmann <henrykielmann@gmail.com>
2014-11-18 22:44:02 +0100

    Enhanced pipe model.

--- example/Pipe/init.lua
+++ example/Pipe/init.lua
@@ -1,10 +1,19 @@
 local class   = require 'middleclass'
 local Model   = require 'core/graphics/Model'
+local MeshBuffer = require 'core/graphics/MeshBuffer'
 local Mesh    = require 'core/graphics/Mesh'
 local Texture = require 'core/graphics/Texture'
 
 
-local pipeMesh = Mesh:load('example/Pipe/Scene.json', 'Pipe')
+local pipeMeshBuffer = MeshBuffer:load('example/Pipe/Scene.json', 'Pipe')
+local scaffoldMeshBuffer = MeshBuffer:load('example/Scaffold/Scene.json', 'Scaffold')
+
+local combinedMeshBuffer = MeshBuffer()
+combinedMeshBuffer:appendMeshBuffer(scaffoldMeshBuffer)
+combinedMeshBuffer:appendMeshBuffer(pipeMeshBuffer)
+
+local combinedMesh = Mesh(combinedMeshBuffer)
+
 local diffuseTexture = Texture:load('2d', 'example/Diffuse.png')
 
 
@@ -12,7 +21,7 @@ local Pipe = class('example/Pipe')
 
 function Pipe:initialize( modelWorld )
     self.model = modelWorld:createModel()
-    self.model:setMesh(pipeMesh)
+    self.model:setMesh(combinedMesh)
     self.model:setProgramFamilyList('simple')
     self.model:setTexture(0, diffuseTexture)
     self.model:setUniform('DiffuseSampler', 0, 'int')
