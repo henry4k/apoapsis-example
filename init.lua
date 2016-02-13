09119784443adb9ae686724ac4ff8d538e9c0b49
Henry Kielmann <henrykielmann@gmail.com>
2016-02-13 14:38:09 +0100

    .

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
-local Vec           = require 'core/Vector'
-local Control       = require 'core/Control'
+local Vec        = require 'core/Vector'
+local Control    = require 'core/Control'
 local GlobalControls = require 'core/GlobalControls'
-local SetupUtils    = require 'base-game/SetupUtils'
-local Background    = require 'base-game/Background'
-local GhostActor    = require 'base-game/world/GhostActor'
-local ScaffoldStructure = require 'base-game/voxel/ScaffoldStructure/init'
+local SetupUtils = require 'base-game/SetupUtils'
+local Background = require 'base-game/Background'
+local GhostActor = require 'base-game/world/GhostActor'
+local Scaffold   = require 'base-game/voxel/Scaffold/init'
 
 
 local function start()
@@ -22,29 +22,37 @@ local function start()
     local chunkManager = SetupUtils.setupChunkManager(voxelVolume, worldModelWorld)
     chunkManager:addActivator(actor.chunkActivator)
 
-    ScaffoldStructure:setupMeshChunkGenerator(chunkManager.meshChunkGenerator)
-    voxelVolume:createStructure(ScaffoldStructure, Vec(0,0,0), 1)
-
-    chunkManager:update()
-
-    local function placeStructure( structureClass, ... )
-        local position = actor.solid:getPosition()
-        local voxelPosition = Vec(math.floor(position[1]),
-                                  math.floor(position[2]),
-                                  math.floor(position[3]))
-        voxelVolume:createStructure(structureClass, voxelPosition, ...)
+    do
+        local voxel = voxelVolume:createVoxelAt(Vec(0,0,0), Scaffold)
+        voxelVolume:setVoxelAt(Vec(0,0,0), voxel)
         chunkManager:update()
+    end
+
+    local function getVoxelPosition()
+        local position = actor.solid:getPosition()
+        return Vec(math.floor(position[1]),
+                   math.floor(position[2]),
+                   math.floor(position[3]))
     end
 
     GlobalControls:mapControl('place-tile', function( self, absolute, delta )
         if delta > 0 then
-            placeStructure(ScaffoldStructure, 0)
+            local position = getVoxelPosition()
+            local voxel = voxelVolume:createVoxelAt(position, Scaffold)
+            voxelVolume:setVoxelAt(position, voxel)
+            chunkManager:update()
         end
     end)
 
     GlobalControls:mapControl('place-tile2', function( self, absolute, delta )
         if delta > 0 then
-            placeStructure(ScaffoldStructure, 1)
+            local position = getVoxelPosition()
+            local voxel = voxelVolume:getVoxelAt(position)
+            if voxel and voxel:isInstanceOf(Scaffold) then
+                voxel:setPlating(1)
+                voxelVolume:setVoxelAt(position, voxel)
+            end
+            chunkManager:update()
         end
     end)
 end
